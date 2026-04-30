bc6940a45d378020cc3e7693ae15271a46d1cda6
ImBruno7 <bruno.motto7@gmail.com>
2026-04-30 16:02:41 -0300

    Agregue el ejercicio 2 de la guia3

---
+++ Guia3/ej2/ej2.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import Guia1.ej1.funciones as f1
+import Guia1.ej2.funcionesej2 as f2
+import Guia3.funcionesG3 as fG3
+
+def main():
+    _, s1 = f1.generar_senoidal(fs=15, fm=100, phi=0, t_inicio=0, t_fin=2)
+    _, s2 = f1.generar_senoidal(fs=15, fm=100, phi=np.pi, t_inicio=0, t_fin=2)
+    A1,A2 = 2,1
+    s1 = A1 * s1
+    s2 = A2 * s2
+    print("\n" + "="*40)
+    print("      Producto Interno")
+    print(f"{np.dot(s1,s2):.3f}")
+
+if __name__ == "__main__":
+    
+    main()
+
+
